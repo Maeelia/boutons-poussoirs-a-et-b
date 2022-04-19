6c76910ab36c40cdf5a8c1178d824b80dab58d7f
Maeelia <103173774+Maeelia@users.noreply.github.com>
2022-04-19 13:20:30 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
